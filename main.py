c25a0d4bc408b9c9665dd40910af8d3e83cb0d54
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-09-05 23:10:50 +0000

    cleaned up code to turn age into an int in its own right

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ age = input("What is your current age?")
 
 #Write your code below this line 👇
 
+age_as_int = int(age)
 
-daysLeft = (90 - int(age)) * 365
-weeksLeft = (90 - int(age)) * 52
-monthsLeft = (90 - int(age)) * 12
+daysLeft = (90 - age_as_int) * 365
+weeksLeft = (90 - age_as_int) * 52
+monthsLeft = (90 - age_as_int) * 12
 
 print(f"You have {daysLeft} days, {weeksLeft} weeks, and {monthsLeft} months left")
 
