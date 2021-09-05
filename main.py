41e49eb45c1c1ca4b8cda665f728f2c960fc8e3a
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-09-05 23:05:56 +0000

    Added all "Left" variables and the print f statement to finish

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ age = input("What is your current age?")
 #Write your code below this line 👇
 
 
+daysLeft = (90 - int(age)) * 365
+weeksLeft = (90 - int(age)) * 52
+monthsLeft = (90 - int(age)) * 12
+
+print(f"You have {daysLeft} days, {weeksLeft} weeks, and {monthsLeft} months left")
 
 
 
